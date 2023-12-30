71b0114e44f9e1f054d5cafe94e097e7c75ad504
RayChen <53119155+RayChen1996@users.noreply.github.com>
2023-12-30 13:22:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,19 +27,30 @@ const upload = multer({ storage: storageEngine });
 
 app.get("/", (req, res) => {
   res.send(`
-    <!DOCTYPE html>
+  <!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Image Upload</title>
+      <!-- Add Bootstrap CDN link here -->
+      <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-      <h1>Image Upload</h1>
-      <form action="/upload" method="post" enctype="multipart/form-data">
-        <input type="file" name="image" accept="image/*" required>
-        <button type="submit">Upload Image</button>
-      </form>
+      <div class="container mt-5">
+        <h1 class="mb-4">Image Upload</h1>
+        <form action="/upload" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="image">Choose Image:</label>
+            <input type="file" class="form-control" name="image" accept="image/*" required>
+          </div>
+          <button type="submit" class="btn btn-primary">Upload Image</button>
+        </form>
+      </div>
+      <!-- Add Bootstrap JS and Popper.js CDN links here (if needed) -->
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
     </html>
   `);
